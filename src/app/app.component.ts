2786fc8d48479b907f407292a4ba67c9ee0c465e
tontonfranki <francisrobitaille@videotron.ca>
2017-03-30 20:28:36 -0400

    Re-initialize repository without old history.

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <hero-detail [hero]="selectedHero"></hero-detail>
+  `,
+  providers: [HeroService]
+})
+export class AppComponent implements OnInit {
+  heroes: Hero[];
+  title = 'Tour of Heroes';
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
